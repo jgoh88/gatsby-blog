1b3ccca8cb2b3c616f43002d1d0b35675dc5dd1a
Jarrod Goh <123610983+jgoh88@users.noreply.github.com>
2023-09-06 15:32:53 +0800

    save

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,27 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "My First Gatsby Site",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `recipe`,
+        path: `${__dirname}/recipe`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 }
